0b99140239ccdf914cf627534432033f1776b662
1noro <ppuubblliicc@protonmail.com>
2022-03-07 17:27:58 +0100

    feat(mysql): :sparkles: agregados nuevos procedures y scripts de prueba

--- database/init/init.sql
+++ database/init/init.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS penadb;
 USE penadb;
 
+
+-- Tables
 CREATE TABLE IF NOT EXISTS sorteo (
     id INT NOT NULL,
     nombre VARCHAR(50) NOT NULL,
@@ -41,6 +43,51 @@ CREATE TABLE IF NOT EXISTS numero (
     FOREIGN KEY (tipo_numero_id) REFERENCES tipo_numero(id)
 );
 
+
+-- Procedures
+DELIMITER $$
+
+    CREATE PROCEDURE insert_boleto_lot_nac_sab(
+        IN _semana_id INT,
+        IN _numero INT
+    ) BEGIN
+        INSERT INTO boleto (sorteo_id, semana_id) VALUES (1, _semana_id);
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_numero, LAST_INSERT_ID(), 1);
+    END;$$
+
+    CREATE PROCEDURE insert_boleto_lot_nac_xob(
+        IN _semana_id INT,
+        IN _numero INT
+    ) BEGIN
+        INSERT INTO boleto (sorteo_id, semana_id) VALUES (2, _semana_id);
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_numero, LAST_INSERT_ID(), 1);
+    END;$$
+
+    CREATE PROCEDURE insert_boleto_primitiva(
+        IN _semana_id INT,
+        IN _numero1 INT,
+        IN _numero2 INT,
+        IN _numero3 INT,
+        IN _numero4 INT,
+        IN _numero5 INT,
+        IN _numero6 INT,
+        IN _reintegro INT
+    ) BEGIN
+        INSERT INTO boleto (sorteo_id, semana_id) VALUES (3, _semana_id);
+        SET @boleto_id = LAST_INSERT_ID();
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_numero1, @boleto_id, 1);
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_numero2, @boleto_id, 1);
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_numero3, @boleto_id, 1);
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_numero4, @boleto_id, 1);
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_numero5, @boleto_id, 1);
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_numero6, @boleto_id, 1);
+        INSERT INTO numero (numero, boleto_id, tipo_numero_id) VALUES (_reintegro, @boleto_id, 2);
+    END;$$
+
+DELIMITER ;
+
+
+-- Datos iniciales
 INSERT IGNORE INTO sorteo (id, nombre, dias, precio) VALUES
     (1, 'Lotaría Nacional (Sábado)', '0000010', 6.0),
     (2, 'Lotaría Nacional (Xoves)', '0001000', 3.0),
---
+++ database/scripts/insert-semana.sql
@@ -0,0 +1,3 @@
+USE penadb;
+
+INSERT INTO semana (id, numero, fecha_lunes) VALUES (202210, 10, '2022-03-07');
