ddc460ee1a6bb4484f82bd1de1454860e1094a38
1noro <ppuubblliicc@protonmail.com>
2022-03-09 22:16:30 +0100

    feat(web): :sparkles: inicio de las bases de la web

--- public/index.php
+++ public/index.php
@@ -1,2 +1,23 @@
-<?php 
-    phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>nooverse.net</title>
+    </head>
+
+    <body style="width: 800px;">
+        <header>
+            <h1>Pena</h1>
+            <hr>
+        </header>
+        <main>
+            <h2>aaa</h2>
+        </main>
+        <footer>
+            <hr>
+            <p>nooverse.net by a stranger <a href="mailto:stranger@nooverse.net">&lt;stranger@nooverse.net&gt;</a></p>
+        </footer>
+    </body>
+</html>
