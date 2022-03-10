e34961d18e25bc85de78545f59350877718c64e9
1noro <ppuubblliicc@protonmail.com>
2022-03-10 18:44:16 +0100

    feat(web): :sparkles: perfilando index

--- database/scripts/insert-semana.sql
+++ database/scripts/insert-semana.sql
@@ -1,3 +1,4 @@
 USE penadb;
 
 INSERT INTO semana (id, numero, fecha_lunes) VALUES (202210, 10, '2022-03-07');
+INSERT INTO semana (id, numero, fecha_lunes) VALUES (202211, 11, '2022-03-14');
--- public/index.php
+++ public/index.php
@@ -1,23 +1,58 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="gl">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <title>nooverse.net</title>
+        <title>Pena</title>
     </head>
-
     <body style="width: 800px;">
         <header>
             <h1>Pena</h1>
             <hr>
         </header>
         <main>
-            <h2>aaa</h2>
+            <h2>Semanas xogadas</h2>
+            <?php
+                setlocale(LC_ALL,"es_ES");
+
+                $servername = "mysql";
+                $username = "readonly";
+                $password = "12345678";
+                $dbname = "penadb";
+
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT id, numero, fecha_lunes FROM semana";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    echo "<ul>";
+                    while($row = $result->fetch_assoc()) {
+                        $numero = $row["numero"];
+                        $fecha = strtotime($row["fecha_lunes"]);
+                        $ano = date("Y", $fecha);
+                        $mes = date("m", $fecha);
+                        $dia = date("d", $fecha);
+                        echo "<li>Semana número $numero do $ano que comeza o lúns $dia/$mes/$ano.</li>";
+                    }
+                    echo "</ul>";
+                } else {
+                    echo "<p>0 resultados 😲</p>";
+                }
+
+                $conn->close();
+            ?>
         </main>
         <footer>
             <hr>
-            <p>nooverse.net by a stranger <a href="mailto:stranger@nooverse.net">&lt;stranger@nooverse.net&gt;</a></p>
+            <p>pena by 1noro<!-- <a href="mailto:example@example.net">&lt;example@example.net&gt;</a>--></p>
         </footer>
     </body>
 </html>
