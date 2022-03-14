321eafbcee602262f240063defde057be061f9d6
1noro <ppuubblliicc@protonmail.com>
2022-03-14 18:48:16 +0100

    chore(mysql): :clown_face: ajustando mocks

--- database/scripts/insert-euromillon.sql
+++ database/scripts/insert-euromillon.sql
@@ -2,4 +2,4 @@ USE penadb;
 
 SET @semana_id = 202210;
 
-CALL insert_boleto_euromillon(@semana_id, 13, 21, 22, 34, 48, 04, 05, 07);
+CALL insert_boleto_euromillon(@semana_id, 13, 25, 34, 43, 48, 04, 05, 07);
