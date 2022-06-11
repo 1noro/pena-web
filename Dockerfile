0ba1708ce33f9a27c7bb9b8d4958c09d6ece6ccf
1noro <ppuubblliicc@protonmail.com>
2022-06-11 16:31:51 +0200

    refactor(infra): :recycle: actualizada estructura de carpetas
    para que cuadre mas con las imagenes

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM nginx AS base
-
-FROM base AS dev
-COPY ./config/nginx/site-dev.conf /etc/nginx/conf.d/site.conf
-
-FROM base AS pro
-COPY ./config/nginx/site-pro.conf /etc/nginx/conf.d/site.conf
-COPY ./public /public
--- scripts/mysql-initialize.sh
+++ scripts/mysql-initialize.sh
@@ -4,12 +4,12 @@ set -e
 
 MYSQL_ROOT_PASSWORD=$1
 
-FILES="database/scripts/semanas/*"
+FILES="pena-mysql/scripts/semanas/*"
 for f in $FILES; do
     filename=${f##*/}
     docker run --rm \
         --network pena-net \
-        -v "$PWD/database/scripts/semanas":/semanas \
+        -v "$PWD/pena-mysql/scripts/semanas":/semanas \
         -v "$PWD/scripts":/scripts \
         -e MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" \
         mysql \
